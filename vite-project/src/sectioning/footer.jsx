b081bd337e1bf7a632fd2be8b4135cdae43dc53b
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-06-05 06:48:25 -0400

    add styling

--- vite-project/src/sectioning/footer.jsx
+++ vite-project/src/sectioning/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div className="y-wrap">Track This!</div>
+      <div className="sticky">Track This!</div>
     </footer>
   )
 };
--- vite-project/src/views/landing.jsx
+++ vite-project/src/views/landing.jsx
@@ -4,8 +4,28 @@ import React from 'react';
 const Landing = () => {
   
   return (
-    <div> I am Landing </div>
+    
+      <>
+      <div>
+    <p className='landingPitch'> Track This is the job hunters "personal assistant" of sorts, helping you discover opportunities, track applications, communicate directly with hiring managers, and much much more.</p>
+     
+      <ul id='featureList'>
+        <li className='feature1'>  Create a favorites list of your desired companies to work for and/or open positions posted on job boards that spark your interest.</li>
+      <br />
+        <li className='feature2'> Keep track of resumes submitted and applications completed.</li>
+      <br />
+        <li className='feature3'> You can even update the position application status for each posting applied for by updating the applied to entry status from applied, to 1st interview, 2nd interview, offer made, etc.
+        </li>
+        <br />
+        </ul>
+        </div>
+    <form className='loginForm'>
+        <button className='lbtn' type="submit">Login</button>
+        <button className='loutbtn' type="submit">Register</button>
+    </form>
+    </>
   );
 };
+
 
 export default Landing;
